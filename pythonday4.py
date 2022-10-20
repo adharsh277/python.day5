b2cce94ec419096f22ce14edd3e50676232b4342
adharsh277 <114822057+adharsh277@users.noreply.github.com>
2022-10-20 20:22:20 +0530

    Add files via upload

---
+++ pythonday4.py
@@ -0,0 +1,63 @@
+from tkinter import Y
+
+
+x = 10, 20, 50, 70, 78
+y = ("ERen", "levi", "sasha", "mikasa", "armin")
+print(x)
+print(y)
+# int
+x = (10)
+y = int(2)
+z = float(10)
+print(x, y, z)
+print(x+y)
+# exercise
+x = 78
+y = ("eren yeger")
+print(type(x))
+print(type(y))
+print(y.replace("eren", "EREN"))
+# slice string
+x = "If You Lose, You Die If You Don't Fight, You Can't Win! So its better do it now other wise you cant win this war"
+y = "I want to see and understand the world outside. I don't want to die inside these walls without knowing what is out there"
+print(x.upper())
+print(x.lower())
+print(y.lower())
+print(y.upper())
+print(type(x))
+print(type(Y))
+print(x.replace("e", "o"))
+print(y.replace("r", "u"))
+print(y.replace("k", "p"))
+print(y.replace("h", "k"))
+print(y.replace("l", "r"))
+print(y.replace("y", "t"))
+print(y.replace("a", "q"))
+print(y.replace("t", "c"))
+print(y.replace("d", "k"))
+print(y.replace("k", "t"))
+print(x.split())
+print(x.split())
+# mylist properties
+mylist = [10, 20, 30, 40, 50, 60, 75, 56]
+mylist[1:2] = [200, 300]
+mylist[1:5] = [200, 300]
+mylist[0:4] = [200, 300]
+mylist[0:6] = [200, 300]
+mylist[1:3] = [200, 300]
+mylist[-6:2] = [200, 300]
+mylist[1:-8] = [200, 300]
+mylist[1:-6] = [200, 300]
+mylist[-1:6] = [200, 300]
+print(mylist)
+mylist.clear()
+print(mylist)
+# arithmetic operator
+print(10+10)
+print(10-20)
+print(100-20)
+print(10*20)
+print(10/20)
+print(10//20)
+print(10 % 20)
+print(10**20)
